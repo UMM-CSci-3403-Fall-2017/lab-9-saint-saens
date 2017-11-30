d35e89f7112863ac03dc2bc897108dab66699a75
cookx876 <cookx876@morris.umn.edu>
2017-11-30 11:08:56 -0600

    Change buffer size to hold the correct amount of data

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -29,53 +29,54 @@ public class Main {
             PacketProcessor file3 = new PacketProcessor();
 
             while(!file1.done || !file2.done || !file3.done){
-                byte[] buf = new byte[1004];
+                byte[] buf = new byte[1028];
                 DatagramPacket receive = new DatagramPacket(buf, buf.length);
                 socket.receive(receive);
 
-                buf = receive.getData();
+                byte[] packet = Arrays.copyOfRange(receive.getData(), 0, receive.getLength());
+                //System.out.println(packet.length);
 
 //                System.out.println("This packet has file id: " + buf[1]);
 
                 //If none of the files (ArrayLists) have been written to
                 if (file1.id == null) {
-                    file1.id = buf[1];
-                    file1.addPacket(buf);
-                    System.out.println("Starting file1 with id: " + file1.id);
+                    file1.id = packet[1];
+                    file1.addPacket(packet);
+//                    System.out.println("Starting file1 with id: " + file1.id);
                 //If the first fileID matched the ID in the packet
-                } else if (file1.id == buf[1]){
-                    file1.addPacket(buf);
+                } else if (file1.id == packet[1]){
+                    file1.addPacket(packet);
 //                    System.out.println("Writing to file1");
                 //If the second file hasn't been written to
                 } else if (file2.id == null) {
-                    file2.id = buf[1];
-                    file2.addPacket(buf);
-                    System.out.println("Starting file2 with id: " + file2.id);
+                    file2.id = packet[1];
+                    file2.addPacket(packet);
+//                    System.out.println("Starting file2 with id: " + file2.id);
                 //If the second fileID matches the packet ID
-                } else if (file2.id == buf[1]){
-                    file2.addPacket(buf);
+                } else if (file2.id == packet[1]){
+                    file2.addPacket(packet);
 //                    System.out.println("Writing to file2");
                 // If the third file hasn't been written to
                 } else if (file3.id == null){
-                    file3.id = buf[1];
-                    file3.addPacket(buf);
-                    System.out.println("Starting file3 with id: " + file3.id);
+                    file3.id = packet[1];
+                    file3.addPacket(packet);
+//                    System.out.println("Starting file3 with id: " + file3.id);
                 // Here if
                 } else {
-                    file3.addPacket(buf);
+                    file3.addPacket(packet);
 //                    System.out.println("packets in file3: " + file3.dataPackets.size() + " length: " + file3.length +
-//                            " done: " + file3.done + " packet index: " + (((buf[2] << 8) & 0xFFFF) ^ (buf[3]& 0xFF)));
+//                            " done: " + file3.done + " packet index: " + (((packet[2] << 8) & 0xFFFF) ^ (packet[3]& 0xFF)));
 //                    System.out.println("Writing to file3");
                 }
             }
 
-            System.out.println("Done getting files");
-
-
+//            System.out.println("Done getting files");
+            
 
             file1.writeFile();
             file2.writeFile();
             file3.writeFile();
+
 
             socket.disconnect();
         } catch (IOException e) {
